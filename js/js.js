38bae3d80c6f7be07418a72a54073183c60bf9be
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-01-28 22:40:37 -1000

    done bitch

--- js/js.js
+++ js/js.js
@@ -1,25 +1,80 @@
-var under = ["isaiah", "isaiah", "zak", "zak", "tan", "tan", "gang", "gang", "21", "21", "yolo", "yolo", "swag", "swag", "braden", "braden"];
-var values = []; 
-var card_ids = [];
-var cardsflipped = 0;
 
-Array.prototype.cards_shuffle = function() {
-	var i = gang.length, g, tan;
-	while (--i > 0){
-		g = Math.floor(Math.random() * (i+1));
-		tan = gang[g];
-		gang[g] = gang[i];
-		gang[i] = tan;
-	}
-	
+function changea(){
+  document.getElementById("a").style.transform = "rotateY(0deg)";
+  document.getElementById("aa").style.transform= "rotateY(-180deg)";
 }
 
- 
-
-function change(ga){
-  document.getElementById(ga).style.backgroundColor="red";
+function changeaa(){
+  document.getElementById("a").style.transform = "rotateY(-180deg)";
+  document.getElementById("aa").style.transform= "rotateY(0deg)";
 }
 
-function changeb(ga){
-  document.getElementById(ga).style.backgroundColor="blue";
+function changeb(){
+  document.getElementById("b").style.transform = "rotateY(0deg)";
+  document.getElementById("bb").style.transform= "rotateY(-180deg)";
+}
+
+function changebb(){
+  document.getElementById("b").style.transform = "rotateY(-180deg)";
+  document.getElementById("bb").style.transform= "rotateY(0deg)";
+}
+
+function changec(){
+  document.getElementById("c").style.transform = "rotateY(0deg)";
+  document.getElementById("cc").style.transform= "rotateY(-180deg)";
+}
+
+function changecc(){
+  document.getElementById("c").style.transform = "rotateY(-180deg)";
+  document.getElementById("cc").style.transform= "rotateY(0deg)";
+}
+
+function changed(){
+  document.getElementById("d").style.transform = "rotateY(0deg)";
+  document.getElementById("dd").style.transform= "rotateY(-180deg)";
+}
+
+function changedd(){
+  document.getElementById("d").style.transform = "rotateY(-180deg)";
+  document.getElementById("dd").style.transform= "rotateY(0deg)";
+}
+
+function changee(){
+  document.getElementById("e").style.transform = "rotateY(0deg)";
+  document.getElementById("ee").style.transform= "rotateY(-180deg)";
+}
+
+function changeee(){
+  document.getElementById("e").style.transform = "rotateY(-180deg)";
+  document.getElementById("ee").style.transform= "rotateY(0deg)";
+}
+
+
+function changef(){
+  document.getElementById("f").style.transform = "rotateY(0deg)";
+  document.getElementById("ff").style.transform= "rotateY(-180deg)";
+}
+
+function changeff(){
+  document.getElementById("f").style.transform = "rotateY(-180deg)";
+  document.getElementById("ff").style.transform= "rotateY(0deg)";
+}
+
+function changeg(){
+  document.getElementById("g").style.transform = "rotateY(0deg)";
+  document.getElementById("gg").style.transform= "rotateY(-180deg)";
+}
+
+function changegg(){
+  document.getElementById("g").style.transform = "rotateY(-180deg)";
+  document.getElementById("gg").style.transform= "rotateY(0deg)";
+}
+function changeh(){
+  document.getElementById("h").style.transform = "rotateY(0deg)";
+  document.getElementById("hh").style.transform= "rotateY(-180deg)";
+}
+
+function changehh(){
+  document.getElementById("h").style.transform = "rotateY(-180deg)";
+  document.getElementById("hh").style.transform= "rotateY(0deg)";
 }
